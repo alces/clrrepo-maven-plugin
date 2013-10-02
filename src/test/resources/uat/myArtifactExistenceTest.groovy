0f9430dcc4eff87faf51bd8e1eb14bd09e6fde60
zalesnyi <zalesnyi@99cadc30-3bf9-11df-9bc1-510916f65ae8>
2013-10-02 13:44:11 +0000

    implement uat on pure AntBulder
    git-svn-id: https://svn/IT/az/clrrepo-maven-plugin@23866 99cadc30-3bf9-11df-9bc1-510916f65ae8

--- src/test/resources/uat/myArtifactExistenceTest.groovy
+++ src/test/resources/uat/myArtifactExistenceTest.groovy
@@ -5,49 +5,52 @@ GRP_ID='${project.groupId}'
 ART_ID='${project.artifactId}'
 VER='${project.version}'
 
-// make test project
 ant = new AntBuilder()
-ant.tempfile(property: 'art.dir', destDir: System.properties['java.io.tmpdir'])
-adir = ant.project.properties['art.dir']
-ant.mkdir dir: adir
-
-pom = new File(adir, 'pom.xml')
 
 try {
-	pom.write("""
-	<project>
-		<modelVersion>4.0.0</modelVersion>
-		<groupId>com.blogspot.cervus_alces</groupId>
-		<artifactId>test_clean</artifactId>
-		<packaging>jar</packaging>
-		<version>1.0-SNAPSHOT</version>
-		<build>
-			<plugins>
-				<plugin>
-					<groupId>$GRP_ID</groupId>
-					<artifactId>$ART_ID</artifactId>
-					<version>$VER</version>
-					<executions>
-						<execution>
-							<goals>
-								<goal>purge</goal>
-							</goals>
-						</execution>
-					</executions>
-				</plugin>			
-			</plugins>
-		</build>
-	</project>
-	""")
+	// make test project
+	ant.sequential {
+		tempfile(property: 'art.dir', destDir: 'target')
+		echo(file: '${art.dir}/pom.xml', message: """
+<project>
+	<modelVersion>4.0.0</modelVersion>
+	<groupId>com.blogspot.cervus_alces</groupId>
+	<artifactId>test_clean</artifactId>
+	<packaging>jar</packaging>
+	<version>1.0-SNAPSHOT</version>
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>$GRP_ID</groupId>
+				<artifactId>$ART_ID</artifactId>
+				<version>$VER</version>
+				<executions>
+					<execution>
+						<goals>
+							<goal>purge</goal>
+						</goals>
+					</execution>
+				</executions>
+			</plugin>			
+		</plugins>
+	</build>
+</project>
+		""")
 
-	repo = new File(adir, 'repository')
-
-	for (skp in [true, false]) {
-		res = "mvn -f $pom.path -Dmaven.repo.local=$repo.path -Dskip.repo.clean=$skp clean".execute().text
-		assert res.contains(' BUILD SUCCESS'), "MAVEN OUTPUT:\n$res"
-		assert new File(repo, GRP_ID.replace('.', '/') + "/$ART_ID/$VER/${ART_ID}-${VER}.jar").exists() == skp, "ARTIFACT EXISTENSE ERROR"
+		for (skp in 1..0) {
+			exec dir: '${art.dir}', failonerror: true, command: "mvn -Dmaven.repo.local=repository -Dskip.repo.clean=${skp.asBoolean()} clean"
+			fail(message: "UNEXPECTED ARTIFACT EXISTENSE (must be $skp)") {
+				condition {
+					not {
+						resourcecount(count: skp) {
+							fileset dir: '${art.dir}/repository', includes: "**/${ART_ID}-${VER}.jar"
+						}
+					}
+				}
+			}
+		}
 	}
 } finally {
 	// cleanup
-	ant.delete dir: adir
+	ant.delete dir: '${art.dir}'
 }
